f0fab9e9f506a1f2402f792a7d997856edf8c188
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-25 14:59:55 -0500

    Double if Statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
         int a = 2;
         int b = 4;
         int x = 6;
-        int y - 1;
+        int y = 1;
 
         System.out.println("a = 2");
         System.out.println("b = 4");
@@ -17,6 +17,21 @@ public class Main {
 
         if(a == x && y + y == a){
             System.out.println("a == x && y + y == a");
+
+            /* && - and - Both conditions Have to be true for this to return true
+               || - or  - Only one condition has to be true for this to return true
+               !  - not - It is not equal to a variable or condition
+               == - equal to - Validates a condition
+             */
+        }
+        if (y + y == a || a == x){
+            System.out.println("y + y == a || a == x");
+        }
+        if (!(a == x) && y + y == a){
+            System.out.println("!(a == x) && y + y == a");
+        }
+        if (a < b){
+            System.out.println("a < b");
         }
     }
 }
